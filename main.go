0c113c8e606963a1a6f1df490a1f1b0d797555cc
Nayan Mallet <nayan.mallet@ynov.com>
2022-11-07 17:05:47 +0100

    ➡️HTTP serveur added ➡️Home template added

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import H "hangman-classic"
+import (
+	"fmt"
+	H "hangman-classic"
+	"net/http"
+)
+
+const port = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bienvenue sur le jeu du pendu !")
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Jouer au pendu !")
+	H.Game()
+}
 
 func main() {
-	H.Game()
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/play", Play)
+
+	fmt.Println("Serveur lancé sur le port", port)
+	http.ListenAndServe(port, nil)
 }
