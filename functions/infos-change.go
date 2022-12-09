59337a73cbfcbdec5ec1a87222446a33a6e4ad72
Nayan Mallet <nayan.mallet@ynov.com>
2022-12-09 14:24:59 +0100

    ➡️rectify problems

--- functions/infos-change.go
+++ functions/infos-change.go
@@ -1,9 +1,12 @@
 package functions
 
+import "strconv"
+
 func WordToPrint(runeArray []rune) string {
 	return string(runeArray)
 }
 
-func PrintMan(lives int) {
-	//
+func PrintMan(lives int) (url string) {
+	url = "http://localhost:8080/static/hangman-steps/" + strconv.Itoa(lives) + ".png"
+	return url
 }
