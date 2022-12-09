a24682eef260cd3a298876c8e443bae2c03d8e6a
Nayan Mallet <nayan.mallet@ynov.com>
2022-12-09 14:21:52 +0100

    ➡️adding sore-board file

--- functions/infos-change.go
+++ functions/infos-change.go
@@ -1,12 +1,9 @@
 package functions
 
-import "strconv"
-
 func WordToPrint(runeArray []rune) string {
 	return string(runeArray)
 }
 
-func PrintMan(lives int) (url string) {
-	url = "http://localhost:8080/static/hangman-steps/" + strconv.Itoa(lives) + ".png"
-	return url
+func PrintMan(lives int) {
+	//
 }
