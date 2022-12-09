626ac2dc6cdda8f516a0115a32a8076916879f7c
Pablo JL~ <pablo.jeanlouis@ynov.com>
2022-12-09 14:34:22 +0100

    ➡editing game.go

--- functions/game.go
+++ functions/game.go
@@ -5,9 +5,6 @@ import (
 )
 
 func Game(Data Infos) Infos {
-	if Data.Propositon == "" {
-		return Data
-	}
 	if Data.Lives > 0 {
 		if string(Data.WordRune) == Data.Word {
 			// TODO: Print Congrats
@@ -45,7 +42,6 @@ func Game(Data Infos) Infos {
 				}
 			}
 		}
-		Data.Propositon = ""
 		return Data
 	}
 	// TODO: Print Loose + The Word
