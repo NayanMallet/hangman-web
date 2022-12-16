faac28ba3ab11ebbf957efedd4f34e76eed3f979
Pablo JL~ <pablo.jeanlouis@ynov.com>
2022-12-16 14:01:25 +0100

    ➡rectifying live problem

--- functions/game.go
+++ functions/game.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Game(Data Infos) Infos {
-	if Data.Lives > 0 {
+	if Data.Lives > 1 {
 		if string(Data.WordRune) == Data.Word {
 			// TODO: Print Congrats
 			Data.WordToPrint = "Congrats !"
@@ -43,8 +43,44 @@ func Game(Data Infos) Infos {
 			}
 		}
 		return Data
+	} else {
+		if string(Data.WordRune) == Data.Word {
+			// TODO: Print Congrats
+			Data.WordToPrint = "Congrats !"
+			return Data
+		}
+		if len(Data.Propositon) > 1 {
+			// case Mot
+			if Data.Propositon == Data.Word {
+				// TODO: Print Congrats
+				Data.WordToPrint = "Congrats !"
+				return Data
+			} else {
+				// TODO: Print Loose + The Word
+				Data.WordToPrint = "You Loose ! The word was " + Data.Word
+				return Data
+			}
+		} else {
+			// case Letter
+			if game_function.ContainsTable(Data.LetterSuggested, Data.Propositon) {
+				// TODO: Letter already proposed
+			} else {
+				if game_function.ContainsString(Data.Word, Data.Propositon) {
+					Data.LetterSuggested = append(Data.LetterSuggested, Data.Propositon)
+					indexes := game_function.LetterInWorld(Data.Word, Data.Propositon)
+					for _, i := range indexes {
+						Data.WordRune[i] = rune(Data.Word[i])
+					}
+					// TODO: Print Proposition + WordToPrint
+					Data.WordToPrint = WordToPrint(Data.WordRune)
+				} else {
+					// TODO: Print Loose + The Word
+					Data.WordToPrint = "You Loose ! The word was " + Data.Word
+					return Data
+				}
+			}
+		}
+		return Data
 	}
-	// TODO: Print Loose + The Word
-	Data.WordToPrint = "You Loose ! The word was " + Data.Word
-	return Data
+
 }
