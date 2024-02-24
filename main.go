fa49ab9f57950882dc6ca8272a5731a6dafab0d7
Nayan Mallet <81246812+NayanMallet@users.noreply.github.com>
2024-02-24 19:09:58 +0100

    Add Handler export function [Vercel deploy need-it]

--- main.go
+++ main.go
@@ -4,26 +4,32 @@ import (
 	"fmt"
 	"hangman-web/functions"
 	"net/http"
+	"os"
 )
 
 const port = ":8080"
 
 func main() {
 	fs := http.FileServer(http.Dir("assets/"))
-	// creates an 'http.FileServe' from the directory "assets/".
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	// The http.StripPrefix function is used to remove the "/static/" prefix from the URL path,
-	// so that the file server can match the correct file in the "assets/" directory.
 
 	http.HandleFunc("/api/", RequestDifficulty)
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/api/hangman", Request)
 	http.HandleFunc("/hangman", Play)
-	// http.HandleFunc function is used to register a function to handle a specific URL path.
 
 	functions.Openbrowser("http://localhost:8080")
-	// Opens the browser at the execution of the program.
 	fmt.Println("Serveur lancé sur le port", port)
+
+	// Use PORT environment variable if available
+	if port := os.Getenv("PORT"); port != "" {
+		port = ":" + port
+	}
+
 	http.ListenAndServe(port, nil)
-	// This line starts the server and listens for incoming HTTP requests
+}
+
+// Handler is the exported function that Vercel looks for
+func Handler(w http.ResponseWriter, r *http.Request) {
+	main()
 }
